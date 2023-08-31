1857c610f167a889bb10bb9d15d96639f9368b9a
Yoonminkim <yoonmin485@naver.com>
2023-08-31 18:18:34 +0900

    08/31 18:18

---
+++ 04. JS/auto_scroll.js
@@ -0,0 +1,44 @@
+
+let pg_num = 0;
+
+let sts_wheel = 0;
+
+let total_page = 6;
+
+setTimeout(()=>{window.scrollTo(0,0)},200);
+
+window.addEventListener('wheel', wheelFn);
+window.addEventListener('DOMContentLoaded', loadFn);
+
+
+const qs = x => document.querySelector(x);
+const qsa = x => document.querySelectorAll(x);
+
+
+function loadFn(){
+
+    console.log('로딩완료');
+
+    total_page = qsa('.page').length;
+    console.log('전체페이지수 : ', total_page);
+
+}
+
+function wheelFn(e){
+
+    if(sts_wheel) return;
+    sts_wheel = 1;
+    setTimeout(()=>{sts_wheel=0},500);
+
+    let delta = e.wheelDelta;
+
+    if(delta<0) pg_num++
+    else pg_num--;
+
+    if(pg_num<0) pg_num = 0;
+    if(pg_num>total_page-1) pg_num = total_page-1;
+
+    console.log('페이지번호:', pg_num);
+    window.scrollTo(0,window.innerHeight*pg_num);
+
+}
