a8b3d9f85260c6655f2ce0d030fa559198f76fb7
Yoonminkim <yoonmin485@naver.com>
2023-09-20 02:15:11 +0900

    09/20 02:14

--- 05. New/Second_page/JS_Second_data.js
+++ 05. New/Second_page/JS_Second_data.js
@@ -3,3 +3,17 @@ const grdpg = qs('.gridbox');
 
 let hcode = '';
 
+for(let i=1;i<10;i++){
+
+    hcode +=
+    `<ul>
+        <li class="grid-item">
+            <img src="../02. image/banner_${i}.jpg">
+        </li>
+    </ul>`
+}
+
+grdpg.innerHTML = hcode;
+
+console.log(hcode);
+
