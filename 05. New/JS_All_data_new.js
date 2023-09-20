fd914aa1bbc821d393ea30b3e94128b4b2f10d73
Yoonminkim <yoonmin485@naver.com>
2023-09-20 16:39:56 +0900

    000

--- 05. New/JS_All_data_new.js
+++ 05. New/JS_All_data_new.js
@@ -4,15 +4,12 @@ const bText2 = '\u00a0'+ 'What is Single Molt';
 const bText3 = '\u00a0'+ 'Do you know Peat?';
 const bText4 = '\u00a0'+ 'More Infomation';
 
-
-
 const banTxt1 = [   // 왼쪽 /////////////////////////////////////////
     bText1, // 배너1에 들어갈 내용
     bText2, // 배너2에 들어갈 내용
     bText3, // 배너3에 들어갈 내용
     bText4 // 배너4에 들어갈 내용
 ];
-
 
 const banTxt2 = [   // 오른쪽 ////////////////////////////////////////
     bText2, // 배너2에 들어간 내용
@@ -21,5 +18,3 @@ const banTxt2 = [   // 오른쪽 ////////////////////////////////////////
     bText1 // 배너1에 들어간 내용
 ];
 
-
-const infoWhiskey = [];
--- 05. New/Second_page/JS_Second_data.js
+++ 05. New/Second_page/JS_Second_data.js
@@ -1,3 +1,4 @@
+import { whiskeyinfo } from "../JS_Whisky_data_new";
 
 const grdpg = qs('.gridbox');
 
@@ -8,12 +9,13 @@ for(let i=1;i<10;i++){
     hcode +=
     `<ul>
         <li class="grid-item">
-            <img src="../02. image/banner_${i}.jpg">
+            <img src="../../02. image/banner_${i}.jpg">
+            <h3 class="whiskey-name">종류명</h3>
         </li>
     </ul>`
 }
 
-grdpg.innerHTML = hcode;
+grdpg.innerHTML = `${hcode}`;
 
 console.log(hcode);
 
