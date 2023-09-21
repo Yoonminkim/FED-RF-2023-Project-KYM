893014002849e135729f2fdb4de2e4652fdc2469
Yoonminkim <yoonmin485@naver.com>
2023-09-21 22:24:59 +0900

    dddd

--- 05. New/JS_navi_new.js
+++ 05. New/JS_navi_new.js
@@ -23,18 +23,48 @@ function showMenu(){
 
 } //////// showMenu //////////////////
 
-
 const mapbtn = qs('.menu-Btn-2');
 const smap = qs('.s-map');
 const cbtn = qs('.cbtn');
+const slide = qsa('.banbox');
+const slide1 = qs('.slide1');
+const slide2 = qs('.slide2');
+
+console.log(slide);
 
 console.log(mapbtn, smap, cbtn);
 
 addEvt(mapbtn, 'click', ()=>{
     smap.classList.add('on');
+    cbtn.classList.add('on');
     document.body.classList.add('on');
 });
 addEvt(cbtn, 'click', ()=>{
     smap.classList.remove('on');
+    cbtn.classList.remove('on');
     document.body.classList.remove('on');
+});
+
+console.log(slide[0]);
+
+const mediaQ = window.matchMedia("(max-width: 850px)")
+
+mediaQ.addListener((e) => {
+let nowWidth = e.media;
+if(e.media < nowWidth){
+
+    console.log(e.media);
+
+    slide[0].classList.remove('col-4');
+    slide[0].classList.add('col-8');
+    slide[1].classList.remove('col-4');
+    slide[1].classList.add('col-0');
+
+}
+else{
+    slide[0].classList.add('col-4');
+    slide[0].classList.remove('col-8');
+    slide[1].classList.add('col-4');
+    slide[1].classList.remove('col-0');
+}
 });
